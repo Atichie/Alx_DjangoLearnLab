c870b0111c3b51f67ac6e118787fd531aae16ebf
Atichie <atichie525@gmail.com>
2024-08-25 14:20:05 +0300

    Add all files

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -11,3 +11,23 @@ def edit_article(request, article_id):
 
     return render(request, 'edit_article.html', {'article': article})
 
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'bookshelf/create_book.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    pass
