5bd972d937761966012433d80d2ed4461527e3bd
Atichie <atichie525@gmail.com>
2024-09-01 12:26:22 +0300

    Add all files

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
 
 urlpatterns = [
         path('books/', BookList.as_view(), name='book-list'),
+        path('', include(router.urls)),
 ]
 
 from rest_framework.routers import DefaultRouter
