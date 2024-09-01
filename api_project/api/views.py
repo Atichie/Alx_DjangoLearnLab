537f51c9495d93249d34bc279231c732f483a6e7
Atichie <atichie525@gmail.com>
2024-09-01 12:20:53 +0300

    Add all files

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -14,3 +14,10 @@ from rest_framework import viewsets
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+from rest_framework.permissions import IsAuthenticated
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
