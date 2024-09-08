8698c87be2da107aaf30e3e6bb283aad5cb63549
Atichie <atichie525@gmail.com>
2024-09-08 16:00:11 +0300

    Add all files

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
+    """
+    Author model stores information about book authors.
+    Each author can have multiple books (one-to-many relationship).
+    """
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
+    """
+    Book model stores details about each book.
+    Each book is linked to one author using a foreign key.
+    """
     title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
