4c90bb65780cdc470378ae8e994122be787dfd33
Atichie <atichie525@gmail.com>
2024-09-10 16:12:22 +0300

    Add all files

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+class BookListView(generics.ListAPIView):
+    """
+    Retrieves a list of all books.
+    This view is open to both authenticated and unauthenticated users.
+    """
+    queryset = BookSerializer
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [generics.ListAPIView)
+    filterset_fields = ['publication_year']
+
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    Retrieves a specific book by its ID.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookCreateView(generics.CreateApiView):
+    """
+    Adds a new book. Restricted to authenticated users.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    Modifies an existing book. Restricted to authenticated users.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+class BookDeleteView(generics.DestroyAPIView):
+    """
+    Delete a book. Restricted to authenticated users.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
