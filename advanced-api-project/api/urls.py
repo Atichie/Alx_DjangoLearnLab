59b1c07b34b47092694148db15411e98b0c5b6af
Atichie <atichie525@gmail.com>
2024-09-10 16:48:37 +0300

    Add all files

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from . import views
 from .views import BookViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
 
 urlpatterns = [
-        path('books/', BookListView.as_view(), name='book-list'),
-        path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-        path('books/create/', BookCreateView.as_view(), name='book-detail'),
-        path('books/<int:pk>update/', BookUpdateView.as_view(), name='book-update'),
-        path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
-        path('', include(router.urls)),
+        path('books/',views.BookListView.as_view(), name='book-list'),
+        path('books/<int:pk>/',views.BookDetailView.as_view(), name='book-detail'),
+        path('books/create/', views.BookCreateView.as_view(), name='book-create'),
+        path('books/<int:pk>update/', view.BookUpdateView.as_view(), name='book-update'),
+        path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),
 ]
