6702e5569980f8c4c8ae0da12377ddeb7eb432d6
Atichie <atichie525@gmail.com>
2024-09-12 20:33:06 +0300

    Add all files

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -104,6 +104,16 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
         'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+X_FRAME_OPTIONS ='DENY'
+
+SECURE_CONTENT_TYPE_NOSNIFF =True
+
+SECURE_BROWSER_XSS_FILTER = True
+
+SESSINON_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
