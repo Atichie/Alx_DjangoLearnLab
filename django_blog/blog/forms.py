2a48c78951f960e0c513ddee3a0790397238ec00
Atichie <atichie525@gmail.com>
2024-09-15 16:04:36 +0300

    Add all files

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+        else:
+            form = UserUpdateForm(instance=request.user)
+        return render(request, 'blog/edit_profile.html', {'form': form})
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+
+urlpatterns = [
+        path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+        path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name= 'logout'),
+]
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+        else:
+            form = CustomUserCreationForm()
+        return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
