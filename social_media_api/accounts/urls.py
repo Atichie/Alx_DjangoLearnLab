355d03507cb74bb2bfe109df17a8e191b0907082
Atichie <atichie525@gmail.com>
2024-09-20 16:29:56 +0300

    Add all files

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
         path('register/', RegisterView.as_view(), name='register'),
         path('login/', CustomObtainAuthToken.as_view(), name='login'),
         path('api/accounts/', include('accounts.urls')),
-        path('follow/<int:pk>', FollowUserView.as_view(), name='follow-user'),
-        path('unfollow/<int:pk>/', UnfollowUserView.as_view(), name='unfollow-user'),
+        path('follow/<int:user_id>', FollowUserView.as_view(), name='follow-user'),
+        path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
