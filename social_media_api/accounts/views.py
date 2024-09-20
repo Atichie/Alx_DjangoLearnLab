dd1e814d405e6e81904dcfa29261dc55b2509270
Atichie <atichie525@gmail.com>
2024-09-20 16:01:08 +0300

    Add all files

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -7,6 +7,8 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from .serializers import UserSerializer
 from .models import CustomUser
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, permissions
 
 class RegisterView(APIView):
     def post(self, request):
@@ -22,3 +24,31 @@ class CustomObtainAuthToken(ObtainAuthToken):
         response = super().post(request, *args, **kwargs)
         token = Token.objects.get(id=token.user_id)
         return Response({'token': token.key, 'user_id': user.id})
+
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        user_to_folllow = get_object_or_404(CustomUser, pk=kwargs['pk'])
+        if user_to_follow != request.user:
+            request.user.following.add(user_to_follow)
+            return Response({"detail": f"Ypu are now following {user_to_follow.username}"})
+        return Response({"detail": "You cannot follow yourself."}, status=400)
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        user_to_unfollow = get_object_or_404(CustomUser, pkkwargs['pk'])
+        if user_to_unfollow in request.user.following.all():
+            request.user.following.remove(user_to_unfollow)
+            return Response({"detail": "You are not following this user."}, status=400)
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,4 +5,7 @@ router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+        path('', views.PostListView.as_view(), name='post-list'),
+        path('<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+]
